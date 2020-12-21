f4ad868792d7d22ba3aca6315e97038edc073043
Maxime Groff <maxime.groff@engie.com>
2020-12-21 16:12:56 +0100

    feat: basic todo app

---
+++ components/TodoForm.js
@@ -0,0 +1,23 @@
+import React, { useState, useContext } from 'react'
+import { TodosContext } from '../contexts/TodosContext'
+
+export default function TodoForm() {
+  const [todo, setTodo] = useState('')
+  const { addTodo } = useContext(TodosContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(todo)
+    setTodo('')
+  }
+
+  return (
+    <form className="form my-6" onSubmit={handleSubmit}>
+      <div className="flex flex-col text-sm mb-2">
+        <label className="font-bold mb-2 text-gray-800" htmlFor="todo">Todo</label>
+        <input type="text" name="todo" id="todo" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="ex. être romantique" className="border border-gray-200 p-2 rounded-lg focus:border-gray-500 appearance-none" />
+      </div>
+      <button type="submit" className="w-full rounded bg-blue-500 text-white hover:bg-blue-600 py-2 px-4">Submit</button>
+    </form>
+  )
+}
---
+++ pages/api/updateTodo.js
@@ -0,0 +1,20 @@
+import { table, getMinifiedRecord } from './utils/Airtable'
+import auth0 from './utils/auth0'
+import OwnsRecord from './middlewares/OwnsRecords'
+
+export default OwnsRecord( async (req, res) => {
+  const { id, fields } = req.body
+  const { user } = await auth0.getSession(req)
+  try {
+    const updatedRecords = await table.update([
+      { id, fields }
+    ])
+    res.statusCode = 200
+    res.json(getMinifiedRecord(updatedRecords[0]))
+  } 
+  catch(err) {
+    console.error(err)
+    res.statusCode = 500
+    res.json({ msg: 'Something went wrong on /api/updateTodo' })
+  }
+})
